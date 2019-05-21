2e0bcc53701f81e00a198d162d91f000ea8fc34c
brychta123 <47573725+brychta123@users.noreply.github.com>
2019-05-21 10:19:22 -0300

    clase 17

---
+++ clase 17/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    /*int* pNumero;
+
+    pNumero =(int*) malloc(sizeof(int));
+   //pNumero =(int*) calloc(sizeof(int),1);//inicializa siempre en 0
+
+*pNumero = 99;
+   printf("heap->pNumero: %d\n", pNumero);
+    printf("stack->&pNumero: %d\n", &pNumero);
+
+  if(pNumero!=NULL)
+  {
+    printf("valor->*pNumero: %d\n", *pNumero);
+   system("pause");
+   printf("luego de hacer un free: \n");
+   free(pNumero);
+
+    printf("heap->&pNumero: %d\n", &pNumero);
+    printf("stack->pNumero: %d\n", pNumero);
+     printf("valor->*pNumero: %d\n", *pNumero);
+  }
+*/
+
+pNumeros=(int*) malloc(sizeof(int)*5)
+
+    return 0;
+}
